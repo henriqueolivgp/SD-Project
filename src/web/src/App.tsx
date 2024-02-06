59b2bb6e8c2ef7e5d4b5a631b80ebfc9a235e35a
Henrique Oliveira <101120107+henriqueolivgp@users.noreply.github.com>
2024-02-06 18:20:33 +0000

    Delete src directory

--- src/web/src/App.tsx
+++
@@ -1,9 +0,0 @@
-export function App() {
-  return (
-    <>
-      <div className="bg-black">
-        <h1 className="text-3xl font-bold underline text-white">Hello world!</h1>
-      </div>
-    </>
-  );
-}
